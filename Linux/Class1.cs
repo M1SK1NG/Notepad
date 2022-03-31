197fbd8f63c5018433681f602dad8facfd3c90d3
Mikhail <88738523+M1SK1NG@users.noreply.github.com>
2022-03-31 13:13:10 +0300

    Add files via upload

---
+++ Linux/Class1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows.Documents;
+using System.Windows.Controls;
+using System.Windows;
+using Linux;
+
+
+
+
+namespace Linux
+{
+    class Class1
+    {
+        string path;
+        string nameFile;
+        RichTextBox fieldEdit;
+        bool result2;
+        public bool Modified { get; set; }
+        public string NameFile
+        {
+            get { return nameFile; }
+        }
+        public Class1(RichTextBox fieldEdit)
+        {
+            nameFile = "";
+
+
+
+            this.fieldEdit = fieldEdit;
+            Modified = false;
+        }
+        public void NewWindow()
+        {
+
+        }
+        public bool ASaveBloknot()
+        {
+
+
+            SaveFileDialog sd = new SaveFileDialog();
+            sd.DefaultExt = "rtf";
+            sd.Filter = "Текстовый файл (*.rtf) |*.rtf|Все файлы(*.*)|*.*";
+            if (nameFile == "")
+                if (sd.ShowDialog() == true)
+                    nameFile = sd.FileName;
+                else return false;
+            TextRange doc = new TextRange(fieldEdit.Document.ContentStart, fieldEdit.Document.ContentEnd);
+            FileStream fs = File.Create(sd.FileName);
+            doc.Save(fs, DataFormats.Rtf);
+            fs.Close();
+            Modified = false;
+            return true;
+        }
+        public void Create()
+        {
+            if (Modified == true)
+            {
+                MessageBoxResult result;
+                //Спросить о сохранении файла ДА/НЕТ/ОТМЕНА
+                result = MessageBox.Show("Вы хотите сохранить изменения в файле", "Сохранение", MessageBoxButton.YesNoCancel,MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+
+
+
+                    //прерываем событие (отмена текущего действия)
+                    if (ASaveBloknot() == false) return;
+                
+                if (result == MessageBoxResult.Cancel) return;
+            }
+            //При успешном развитии событий
+            fieldEdit.Document.Blocks.Clear();//Ouvuaem 6n0KHOT
+            nameFile = "";//Очищаем имя файла
+            Modified = false;//Сброс признака редактирования
+        }
+        public void SaveNoAccess()
+        {
+            path = Path.GetDirectoryName(NameFile);
+
+
+
+
+
+            TextRange documentTextRange = new TextRange(fieldEdit.Document.ContentStart, fieldEdit.Document.ContentEnd);
+
+            using (FileStream fs = File.Create(nameFile))
+            {
+                if (System.IO.Path.GetExtension(nameFile).ToLower() == ".rtf")
+                {
+                    documentTextRange.Save(fs, DataFormats.Text);
+
+                }
+            }
+
+
+        }
+        public void Save()
+
+        {
+            if (File.Exists(NameFile))
+            {
+                path = Path.GetDirectoryName(NameFile);
+
+
+
+
+                {
+                    MessageBoxResult result1;
+
+
+                    result1 = MessageBox.Show("Вы желаете сохранить изминения?", "Сохранение", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                    if (result1 == MessageBoxResult.Yes)
+                    {
+                        TextRange documentTextRange = new TextRange(fieldEdit.Document.ContentStart, fieldEdit.Document.ContentEnd);
+
+                        using (FileStream fs = File.Create(nameFile))
+                        {
+                            if (System.IO.Path.GetExtension(nameFile).ToLower() == ".rtf")
+                            {
+                                documentTextRange.Save(fs, DataFormats.Text);
+                                result2 = true;
+                            }
+                        }
+
+                    }
+                    if (result1 == MessageBoxResult.No)
+                    {
+                        result2 = false;
+                    }
+                   
+
+                }
+
+
+            }
+
+            else
+            {
+
+                MessageBoxResult result;
+
+                result = MessageBox.Show("Вы желаете сохранить новый файл?", "Сохранение", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    ASaveBloknot();
+
+                }
+                if (result == MessageBoxResult.No)
+                {
+
+                }
+            }
+
+
+        }
+        public void FollowingPose()
+        {
+           
+            fieldEdit.CaretPosition = fieldEdit.CaretPosition.GetNextInsertionPosition(LogicalDirection.Forward);
+        }
+
+        public void DeleteThisLine()
+        {
+            
+            fieldEdit.CaretPosition = fieldEdit.CaretPosition.GetNextContextPosition(LogicalDirection.Forward);
+            fieldEdit.CaretPosition.DeleteTextInRun(-250);
+            
+        }
+        public void OpenFile()
+        {
+            
+            if (Modified == true)
+            {
+                if (File.Exists(NameFile))
+                {
+                    MessageBoxResult result;
+                    if (result2 == false)
+                    {
+                        result = MessageBox.Show("Вы хотите сохранить изменения?", "Сохранение", MessageBoxButton.YesNo,MessageBoxImage.Question);
+
+                        if (result == MessageBoxResult.Yes)
+                        {
+                            SaveNoAccess();
+                            OpenFileDialog ofd = new OpenFileDialog();
+                            ofd.Filter = "RichText Files (*.rtf)|*.rtf|All files (*.*)|*.*";
+
+                            if (ofd.ShowDialog() == true)
+                            {
+                                nameFile = ofd.FileName;
+                                TextRange doc = new TextRange(fieldEdit.Document.ContentStart, fieldEdit.Document.ContentEnd);
+                                using (FileStream fs = new FileStream(ofd.FileName, FileMode.Open))
+                                {
+                                    if (Path.GetExtension(ofd.FileName).ToLower() == ".rtf")
+                                        doc.Load(fs, DataFormats.Rtf);
+                                    else if (Path.GetExtension(ofd.FileName).ToLower() == ".txt")
+                                        doc.Load(fs, DataFormats.Text);
+                                    else
+                                        doc.Load(fs, DataFormats.Xaml);
+                                }
+                            }
+                        }
+                        if (result == MessageBoxResult.No)
+                        {
+                            OpenFileDialog ofd = new OpenFileDialog();
+                            ofd.Filter = "RichText Files (*.rtf)|*.rtf|All files (*.*)|*.*";
+
+                            if (ofd.ShowDialog() == true)
+                            {
+                                nameFile = ofd.FileName;
+                                TextRange doc = new TextRange(fieldEdit.Document.ContentStart, fieldEdit.Document.ContentEnd);
+                                using (FileStream fs = new FileStream(ofd.FileName, FileMode.Open))
+                                {
+                                    if (Path.GetExtension(ofd.FileName).ToLower() == ".rtf")
+                                        doc.Load(fs, DataFormats.Rtf);
+                                    else if (Path.GetExtension(ofd.FileName).ToLower() == ".txt")
+                                        doc.Load(fs, DataFormats.Text);
+                                    else
+                                        doc.Load(fs, DataFormats.Xaml);
+                                }
+                            }
+                        }
+                    }
+                    if (result2 == true)
+                    {
+                        SaveNoAccess();
+                    }           
+                }
+                    if (!File.Exists(NameFile))
+                    {
+                        MessageBoxResult result3;
+                        result3 = MessageBox.Show("Вы хотите сохранить новый документ?", "Сохранить как", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+                        if (result3 == MessageBoxResult.Yes)
+                        {
+                            ASaveBloknot();
+                        OpenFileDialog ofd = new OpenFileDialog();
+                        ofd.Filter = "RichText Files (*.rtf)|*.rtf|All files (*.*)|*.*";
+
+                        if (ofd.ShowDialog() == true)
+                        {
+                            nameFile = ofd.FileName;
+                            TextRange doc = new TextRange(fieldEdit.Document.ContentStart, fieldEdit.Document.ContentEnd);
+                            using (FileStream fs = new FileStream(ofd.FileName, FileMode.Open))
+                            {
+                                if (Path.GetExtension(ofd.FileName).ToLower() == ".rtf")
+                                    doc.Load(fs, DataFormats.Rtf);
+                                else if (Path.GetExtension(ofd.FileName).ToLower() == ".txt")
+                                    doc.Load(fs, DataFormats.Text);
+                                else
+                                    doc.Load(fs, DataFormats.Xaml);
+                            }
+                        }
+                    }
+                        if (result3 == MessageBoxResult.No)
+                        {
+                        OpenFileDialog ofd = new OpenFileDialog();
+                        ofd.Filter = "RichText Files (*.rtf)|*.rtf|All files (*.*)|*.*";
+
+                        if (ofd.ShowDialog() == true)
+                        {
+                            nameFile = ofd.FileName;
+                            TextRange doc = new TextRange(fieldEdit.Document.ContentStart, fieldEdit.Document.ContentEnd);
+                            using (FileStream fs = new FileStream(ofd.FileName, FileMode.Open))
+                            {
+                                if (Path.GetExtension(ofd.FileName).ToLower() == ".rtf")
+                                    doc.Load(fs, DataFormats.Rtf);
+                                else if (Path.GetExtension(ofd.FileName).ToLower() == ".txt")
+                                    doc.Load(fs, DataFormats.Text);
+                                else
+                                    doc.Load(fs, DataFormats.Xaml);
+                            }
+                        }
+                    }
+                        if (result3 == MessageBoxResult.Cancel)
+                        {
+                            
+                        }
+                }
+                
+            }
+            
+        }
+    }
+}     
+            
+                    
+                
+
+            
+         
+            
+           
+        
+    
+
---
+++ Linux/MainWindow.xaml.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Linux
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Class1 bloknot;
+        public MainWindow()
+        {
+            InitializeComponent();
+            bloknot = new Class1(Rich1);
+        }
+        DispatcherTimer timer;
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime d = DateTime.Now;
+            time.Text = d.ToString("HH:mm:ss");
+            data.Text = d.ToString("dd:MM:yyyy");
+        }
+        private void CreateS_Click(object sender, RoutedEventArgs e)
+        {
+            bloknot.Create();
+            this.Title = bloknot.NameFile;
+        }
+        private void Rich1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bloknot.Modified=true;
+        }
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            
+            bloknot.OpenFile();
+            this.Title=bloknot.NameFile;
+        }
+        private void EXTS_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void ASSAve_Click(object sender, RoutedEventArgs e)
+        {
+            bloknot.ASaveBloknot();
+            this.Title = bloknot.NameFile;
+        }
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (bloknot.Modified == true)
+                bloknot.Save();
+            this.Title = bloknot.NameFile;
+
+        }
+        private void copy_Click(object sender, RoutedEventArgs e)
+        {
+            Rich1.Copy();
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (bloknot.Modified == true)
+            {
+                MessageBoxResult result;
+                result = MessageBox.Show("Вы желаете сохранить файл?", "Блокнот", MessageBoxButton.YesNoCancel,MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                    if (bloknot.ASaveBloknot() == false) return;
+
+
+                if (result == MessageBoxResult.Cancel) return;
+
+            }
+            else
+            {
+
+            }
+
+        }
+        private void Paste_Click(object sender, RoutedEventArgs e)
+        {
+            Rich1.Paste();
+        }
+        private void Cut_Click(object sender, RoutedEventArgs e)
+        {
+            Rich1.Cut();
+        }
+        private void NewWindow_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+        }
+        private void remove_Click(object sender, RoutedEventArgs e)
+        {
+
+            Rich1.Selection.Text = "";
+        
+
+        }
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Rich1.Undo();
+        }
+        private void AllSelect_Click(object sender, RoutedEventArgs e)
+        {
+            Rich1.SelectAll();
+        }
+        private void Time_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(" Дата:" + DateTime.Now.ToShortDateString()+ "\n " +"Актуальное время:" + DateTime.Now.ToLongTimeString());
+        }
+        private void Fat_Click(object sender, RoutedEventArgs e)
+        {
+
+            object temp = Rich1.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            if (temp != DependencyProperty.UnsetValue && temp.Equals(FontWeights.Normal))
+            {
+                Rich1.Selection.ApplyPropertyValue(Inline.FontWeightProperty,FontWeights.Bold);
+
+            }
+            else
+            {
+                Rich1.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Normal);
+            }
+
+        }
+        private void Italic_Click(object sender, RoutedEventArgs e)
+        {
+            object temp = Rich1.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            if (temp != DependencyProperty.UnsetValue && temp.Equals(FontStyles.Normal))
+            {
+                Rich1.Selection.ApplyPropertyValue(Inline.FontStyleProperty, FontStyles.Italic);
+
+            }
+            else
+            {
+                Rich1.Selection.ApplyPropertyValue(Inline.FontStyleProperty, FontStyles.Normal);
+            }
+        }
+        private void UnderLine_Click(object sender, RoutedEventArgs e)
+        {
+            if (Rich1.Selection.GetPropertyValue(Inline.TextDecorationsProperty) != TextDecorations.Underline)
+            {
+                Rich1.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
+
+            }
+            else
+            {
+                Rich1.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, null);
+            }   
+        }
+        private void DeleteLine_Click(object sender, RoutedEventArgs e)
+        {
+            bloknot.DeleteThisLine();
+        }
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.bing.com/search?q=%d1%81%d0%bf%d1%80%d0%b0%d0%b2%d0%ba%d0%b0+%d0%bf%d0%be+%d0%b8%d1%81%d0%bf%d0%be%d0%bb%d1%8c%d0%b7%d0%be%d0%b2%d0%b0%d0%bd%d0%b8%d1%8e+%d0%b1%d0%bb%d0%be%d0%ba%d0%bd%d0%be%d1%82%d0%b0+%d0%b2+windows%c2%a010&filters=guid:%224466414-ru-dia%22%20lang:%22ru%22&form=T00032&ocid=HelpPane-BingIA");
+        }
+        private void NextPose_Click(object sender, RoutedEventArgs e)
+        {
+            bloknot.FollowingPose();
+        }
+        private void StatusBars_Click(object sender, RoutedEventArgs e)
+        {
+            if (Status.Visibility == Visibility.Visible)
+            {
+                Status.Visibility = Visibility.Hidden;
+                Rich1.Margin = new Thickness(0, 18, 0, 22);
+            }
+            else
+            {
+                Status.Visibility = Visibility.Visible;
+                Rich1.Margin = new Thickness(0, 18, 0, 22);
+            }
+        }
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 0, 1, 0);
+            timer.IsEnabled = true;
+        }
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Разработал программу блокнот:\nСтудент группы:ИСП-31\nЛейбович Михаил\nЗадание: Разработать блокнот\nДоп.Задание по варианту №2:\n1)Перейти в новую позицию.\n2)Удалить текущую строку.\n\nЗащещено не авторскими правми©");
+        }
+    }
+}
